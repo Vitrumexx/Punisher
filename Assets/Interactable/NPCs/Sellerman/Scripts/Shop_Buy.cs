31a3d44c2e8972ca2e841ab92af413ad128a0914
Vitrumexx <aleshkevichnikita@gmail.com>
2025-10-28 00:16:39 +0300

    Update for shop
    Input Field for amount of staff player wants to buy or sell added

--- Assets/Interactable/NPCs/Sellerman/Scripts/Shop_Buy.cs
+++ Assets/Interactable/NPCs/Sellerman/Scripts/Shop_Buy.cs
@@ -9,12 +9,13 @@ public class Shop_Buy : MonoBehaviour
     public PlayerParameters playerParam;
     public InventoryManager inventory;
     public ItemScriptableObject item;
-
+    private TMP_InputField amount;
     public int price;
     public Image image;
     public TextMeshProUGUI TMPtext;
     void Start()
     {
+        amount = GetComponentInChildren<TMP_InputField>();
         image.sprite = item.Icon;
         TMPtext.text = price.ToString();
     }
@@ -27,19 +28,38 @@ public class Shop_Buy : MonoBehaviour
             inventory.AddItem(item, 1);
             Debug.Log(item.name + " has been sold for " + price);
         }
-        else
-            return;
+        else return;
     }
 
     public void SellAmmo()
     {
-        if (playerParam._money >= price)
+        if (amount == null)
         {
-            playerParam._money -= price;
-            playerParam._bullets++;
-            Debug.Log(item.name + " has been sold for " + price);
-        }
-        else
+            Debug.LogWarning("InputField не найден!");
             return;
+        }
+
+        string textValue = amount.text.Trim();
+        if (string.IsNullOrEmpty(textValue))
+        {
+            Debug.LogWarning("Поле ввода пустое!");
+            return;
+        }
+
+        if (!int.TryParse(textValue, out int IntValue))
+        {
+            Debug.LogWarning("Некорректное значение, нужно ввести число!");
+            return;
+        }
+
+        int totalPrice = price * IntValue;
+
+        if (playerParam._money >= totalPrice)
+        {
+            playerParam._money -= totalPrice;
+            playerParam._bullets += IntValue;
+            Debug.Log($"{IntValue} {item.name} продано за {totalPrice}");
+        }
+        else Debug.Log("Недостаточно денег!");
     }
 }
--- Assets/Interactable/NPCs/Sellerman/Scripts/Shop_Sell.cs
+++ Assets/Interactable/NPCs/Sellerman/Scripts/Shop_Sell.cs
@@ -10,21 +10,43 @@ public class Shop_Sell : MonoBehaviour
     public int price;
     public Image image;
     public TextMeshProUGUI TMPtext;
+    private TMP_InputField amount;
     void Start()
     {
+        amount = GetComponentInChildren<TMP_InputField>();
         image.sprite = item.Icon;
         TMPtext.text = price.ToString();
     }
 
     public void BuyFromPlayer()
     {
-        if (playerParam._supplies >= 1)
+        if (amount == null)
         {
-            playerParam._money += playerParam._supplies;
-            playerParam._supplies = 0;
+            Debug.LogWarning("InputField не найден!");
+            return;
+        }
+
+        string textValue = amount.text.Trim();
+        if (string.IsNullOrEmpty(textValue))
+        {
+            Debug.LogWarning("Поле ввода пустое!");
+            return;
+        }
+
+        if (!int.TryParse(textValue, out int IntValue))
+        {
+            Debug.LogWarning("Некорректное значение, нужно ввести число!");
+            return;
+        }
+
+        int totalPrice = price * IntValue;
+
+        if (playerParam._supplies >= IntValue)
+        {
+            playerParam._money += totalPrice;
+            playerParam._supplies -= IntValue;
             Debug.Log(item.name + " has been sold for " + price);
         }
-        else
-            return;
+        else Debug.Log("Недостаточно материалов!");
     }
 }
